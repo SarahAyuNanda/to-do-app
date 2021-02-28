3cfa7ba7ccf0becd598ff7afa8062658a35a0cf9
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-02-28 16:30:33 +0700

    Import header style

--- src/container/index.js
+++ src/container/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppHeader from '../component/header';
+import AppContent from '../component/content';
 import './index.css'
 
 const ToDoApp = () => {
     return (
         <div className='app-container'>
             <AppHeader />
+            <AppContent />
         </div>
     );
 };
