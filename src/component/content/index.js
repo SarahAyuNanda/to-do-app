96f1ed77e9a4156a31cee741bbac2a71935891c5
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-02-28 17:41:28 +0700

    Create first state and handler for input and button

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input } from 'semantic-ui-react';
 import './index.css';
 
 const AppContent = () => {
+    const [item, setItem] = useState('')
+    const [itemList, setItemList] = useState([])
+
+    const actionInput = {
+        color: 'blue',
+        labelPosition: 'right',
+        icon: 'add',
+        content: 'Add',
+        onClick: () => onClickAddButton()
+    }
+
+    const onChangeInput = (event, data) => {
+        setItem(event.target.value)
+    }
+
+    const onClickAddButton = () => {
+        let store = [...itemList]
+        store.push(item)
+        setItemList(store)
+    }
+
     return (
         <div className='app-content'>
-
+            <Input
+                className='input-list'
+                action={actionInput}
+                onChange={onChangeInput}
+            />
+            {itemList.join(', ')}
         </div>
     );
 };
