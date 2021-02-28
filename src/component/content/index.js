1a0e12b5be98a7a06e4bb63ba8278d4c2155a886
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-02-28 21:06:47 +0700

    Change component to child

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -1,62 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Card, Icon, Input } from 'semantic-ui-react';
+import InputItem from './input';
+import CardItem from './card';
 import './index.css';
 
 const AppContent = () => {
     const [item, setItem] = useState('')
     const [itemList, setItemList] = useState([])
 
-    const actionInput = {
-        color: 'blue',
-        labelPosition: 'right',
-        icon: 'add',
-        content: 'Add',
-        onClick: () => onClickAddButton()
-    }
-
-    const onChangeInput = (event, data) => {
-        if (event.key === 'Enter') {
-            onClickAddButton()
-        }
-        setItem(event.target.value)
-    }
-
-    const onClickAddButton = () => {
-        let store = [...itemList]
-        store.push(item)
-        setItemList(store)
-    }
-
     return (
         <div className='app-content'>
-            <Input
-                className='input-list'
-                action={actionInput}
-                onKeyUp={onChangeInput}
-            />
-            <Card.Group itemsPerRow={2}>
-                {itemList.map((item, index) => {
-                    return (
-                        <Card>
-                            <Card.Content>
-                                <Card.Description>{item}</Card.Description>
-                                <Card.Description className='action-list'>
-                                    <Button  color='yellow'>
-                                        <Button.Content>
-                                            <Icon name='edit' />
-                                        </Button.Content>
-                                    </Button>
-                                    <Button color='red' >
-                                        <Button.Content>
-                                            <Icon name='trash' />
-                                        </Button.Content>
-                                    </Button>
-                                </Card.Description>
-                            </Card.Content>
-                        </Card>
-                    )
-                })}
-            </Card.Group>
+            <InputItem item={item} itemList={itemList} setItem={setItem} setItemList={setItemList} />
+            <CardItem itemList={itemList} setItemList={setItemList} />
         </div>
     );
 };
