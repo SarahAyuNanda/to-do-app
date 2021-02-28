95781cac4c8fe646d996b3d1d262a6fef8b70bc3
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-02-28 12:40:00 +0700

    Change init app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ToDoApp from './container';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
