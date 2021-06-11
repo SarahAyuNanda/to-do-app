332e62989858fd72a0846aeaefa9c03c322e9b07
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-06-11 09:26:32 +0700

    Add function and state for checking item

--- src/component/content/card/index.js
+++ src/component/content/card/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card, Icon } from 'semantic-ui-react';
+import { Button, Card, Checkbox, Icon } from 'semantic-ui-react';
 import './index.css';
 
 const CardItem = props => {
     const { itemList, setItemList } = props
+    const { itemCheck, setItemCheck } = useState(false)
+
+    const onCheckButton = event => {
+
+    }
 
     const onClickEditButton = (event, data) => {
         console.log(data);
     }
-    
+
     const onClickDeleteButton = (event, data) => {
         let store = [...itemList]
         store.splice(data, 1)
@@ -17,12 +22,19 @@ const CardItem = props => {
     }
 
     return (
-        <Card.Group itemsPerRow={2}>
+        <Card.Group itemsPerRow={3}>
             {itemList.map((item, index) => {
                 return (
                     <Card>
                         <Card.Content>
-                            <Card.Description>{item}</Card.Description>
+                            <Card.Description>
+                                <Checkbox
+                                    className="check-list"
+                                    color="primary"
+                                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                                />
+                            </Card.Description>
+                            <Card.Description className="item-list">{item}</Card.Description>
                             <Card.Description className='action-list'>
                                 <Button color='yellow' onClick={event => onClickEditButton(event, index)}>
                                     <Button.Content>
