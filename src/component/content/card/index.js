4017e38e98b61e5cc492ce45461c2baafbf3157a
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-06-20 11:38:11 +0700

    Reformat card component

--- src/component/content/card/index.js
+++ src/component/content/card/index.js
@@ -1,75 +1,106 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, Checkbox, Divider, Icon } from 'semantic-ui-react';
 import './index.css';
 
 const CardItem = props => {
-    const { itemList, setItemList } = props
+    const { 
+        itemList, 
+        itemCheckedList, 
+        onClickEditButton, 
+        onClickDeleteButton, 
+        onCheckButton 
+    } = props
 
     const onChangeStrikeThrough = value => {
         return value ? "line-through" : "none"
     }
 
-    const onCheckButton = index => {
-        let filterCheckedItem = itemList.map((item, i) => {
-            if (i === index) {
-                item.isChecked = !item.isChecked;
-            }
-            return item;
-        });
-        setItemList(filterCheckedItem);
-    }
-
-    const onClickEditButton = (event, data) => {
-        console.log(data);
-    }
-
-    const onClickDeleteButton = (event, index) => {
-        let storeItem = [...itemList]
-        storeItem.splice(index, 1)
-        setItemList(storeItem)
-    }
-
     return (
-        <Card.Group itemsPerRow={3}>
-            {itemList.map((item, index) => {
-                const { label, isChecked } = item
-                return (
-                    <Card key={index}>
-                        <Card.Content>
-                            <Card.Description>
-                                <Checkbox
-                                    id={index}
-                                    className="check-list"
-                                    color="primary"
-                                    checked={isChecked}
-                                    onClick={() => onCheckButton(index, item)}
-                                />
-                            </Card.Description>
-                            <Card.Description className="item-list" style={{ "textDecoration": onChangeStrikeThrough(isChecked, index) }}>{label}</Card.Description>
-                            <Card.Description className='action-list'>
-                                <Button color='yellow' onClick={event => onClickEditButton(event, index)}>
-                                    <Button.Content>
-                                        <Icon name='edit' />
-                                    </Button.Content>
-                                </Button>
-                                <Button color='red' onClick={event => onClickDeleteButton(event, index)}>
-                                    <Button.Content>
-                                        <Icon name='trash' />
-                                    </Button.Content>
-                                </Button>
-                            </Card.Description>
-                        </Card.Content>
-                    </Card>
-                )
-            })}
-        </Card.Group>
+        <div>
+            <Card.Group itemsPerRow={3}>
+                {itemList.map((item, index) => {
+                    const { label, isChecked } = item
+                    return (
+                        <Card key={index}>
+                            <Card.Content>
+                                <Card.Description>
+                                    <Checkbox
+                                        id={index}
+                                        className="check-list"
+                                        color="primary"
+                                        checked={isChecked}
+                                        onClick={() => onCheckButton(index)}
+                                    />
+                                </Card.Description>
+                                <Card.Description className="item-list" style={{ "textDecoration": onChangeStrikeThrough(isChecked, index) }}>{label}</Card.Description>
+                                <Card.Description className='action-list'>
+                                    <Button color='yellow' onClick={event => onClickEditButton(event, index)}>
+                                        <Button.Content>
+                                            <Icon name='edit' />
+                                        </Button.Content>
+                                    </Button>
+                                    <Button color='red' onClick={() => onClickDeleteButton(index)}>
+                                        <Button.Content>
+                                            <Icon name='trash' />
+                                        </Button.Content>
+                                    </Button>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    )
+                })}
+            </Card.Group>
+            {itemCheckedList.length ?
+                <div>
+                    <Divider />
+                    <Card.Group itemsPerRow={3}>
+                        {itemCheckedList.map((item, index) => {
+                            const { label, isChecked } = item
+                            return (
+                                <Card key={index}>
+                                    <Card.Content>
+                                        <Card.Description>
+                                            <Checkbox
+                                                id={index}
+                                                className="check-list"
+                                                color="primary"
+                                                checked={isChecked}
+                                                onClick={() => onCheckButton(index)}
+                                            />
+                                        </Card.Description>
+                                        <Card.Description className="item-list" style={{ "textDecoration": onChangeStrikeThrough(isChecked, index) }}>{label}</Card.Description>
+                                        <Card.Description className='action-list'>
+                                            <Button color='yellow' onClick={event => onClickEditButton(event, index)}>
+                                                <Button.Content>
+                                                    <Icon name='edit' />
+                                                </Button.Content>
+                                            </Button>
+                                            <Button color='red' onClick={event => onClickDeleteButton(event, index)}>
+                                                <Button.Content>
+                                                    <Icon name='trash' />
+                                                </Button.Content>
+                                            </Button>
+                                        </Card.Description>
+                                    </Card.Content>
+                                </Card>
+                            )
+                        })}
+                    </Card.Group>
+                </div>
+                :
+                <div />
+            }
+        </div>
     );
 };
 
 CardItem.propTypes = {
     itemList: PropTypes.array,
-    setItemList: PropTypes.any,
+    itemCheckedList: PropTypes.array,
+    onClickEditButton: PropTypes.func,
+    onClickDeleteButton: PropTypes.func,
+    onCheckButton: PropTypes.func
 };
 
 export default CardItem;
